3e5f2c2cee1e877fd04aa898b5a03df1daac3469
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2024-11-19 01:15:06 +0300

    Project architecture is being designed Step-2

--- Paragraph.Core/obj/Debug/net8.0/Paragraph.Core.AssemblyInfo.cs
+++ Paragraph.Core/obj/Debug/net8.0/Paragraph.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Paragraph.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3e1e6644d4c654ab846cb4117b975783cd3188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe791b69af85f5e304d1bcf7f8a286abab280f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Paragraph.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Paragraph.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

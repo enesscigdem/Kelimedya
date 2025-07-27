587afc07123264558973f708e2ae3ef13f336ab0
Enes Çiğdem <55703841+enesscigdem@users.noreply.github.com>
2025-07-27 16:41:06 +0300

    Fix course progress aggregation

--- Kelimedya.WebApp/Areas/Student/Controllers/DashboardController.cs
+++ Kelimedya.WebApp/Areas/Student/Controllers/DashboardController.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Kelimedya.Core.Enum;
 using Kelimedya.WebApp.Areas.Student.Models;
+using Kelimedya.WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace Kelimedya.WebApp.Areas.Student.Controllers
@@ -26,6 +27,16 @@ namespace Kelimedya.WebApp.Areas.Student.Controllers
             var studentId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             var viewModel = await _httpClient.GetFromJsonAsync<StudentDashboardViewModel>($"api/student/dashboard/{studentId}");
+
+            try
+            {
+                var userInfo = await _httpClient.GetFromJsonAsync<UserInfoViewModel>($"api/users/{studentId}");
+                ViewBag.FullName = userInfo?.FullName;
+            }
+            catch (HttpRequestException)
+            {
+                ViewBag.FullName = null;
+            }
 
             return View(viewModel);
         }

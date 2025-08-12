9ab544ab5d3120a51b7b760788bc9733fb3fed12
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2025-08-12 16:42:43 +0300

    update

--- Kelimedya.WebApp/obj/Debug/net8.0/Kelimedya.WebApp.AssemblyInfo.cs
+++ Kelimedya.WebApp/obj/Debug/net8.0/Kelimedya.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kelimedya.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9c612daaad005fca6b3173d58a9772ad158bfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b421898bcce6aa61af491f7fad4c2ad3709ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kelimedya.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kelimedya.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

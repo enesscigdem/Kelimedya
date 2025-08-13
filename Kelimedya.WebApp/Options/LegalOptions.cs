dba36169f1ca199db9fe0e64cd1902a1edf89fe0
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2025-08-13 17:39:50 +0300

    Add initial files for TinyMCE integration and update legal information

--- Kelimedya.WebApp/Options/LegalOptions.cs
+++ Kelimedya.WebApp/Options/LegalOptions.cs
@@ -5,7 +5,6 @@ namespace Kelimedya.WebApp.Options
         public string CompanyName { get; set; } = "";
         public string TaxOffice { get; set; } = "";
         public string TaxNo { get; set; } = "";
-        public string Mersis { get; set; } = "";
         public string Address { get; set; } = "";
         public string Phone { get; set; } = "";
         public string Email { get; set; } = "";
